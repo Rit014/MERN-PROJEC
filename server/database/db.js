326b3cc248a668bb3a35cc36202f5ea8faa3fa01
Rit014 <ritu72052@gmail.com>
2025-04-21 14:42:03 +0530

    Your commit message here

--- server/database/db.js
+++ server/database/db.js
@@ -3,18 +3,19 @@ import dotenv from "dotenv";
 
 dotenv.config(); 
 
+const URL = process.env.MONGODB_URI || 
+  `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@blog.znl8106.mongodb.net/myDatabase?retryWrites=true&w=majority`;
+
 const Connection = async () => {
-    const URL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@blog.znl8106.mongodb.net/myDatabase?retryWrites=true&w=majority`;
-    try {
-        await mongoose.connect(URL);
-        console.log("✅ Database connected successfully!");
-    } catch (error) {
-        console.error("❌ Error while connecting with the database:", error.message);
-    }
+  try {
+    await mongoose.connect(URL);
+    console.log("✅ Database connected successfully!");
+  } catch (error) {
+    console.error("❌ Error while connecting with the database:", error.message);
+  }
 };
 
 const conn = mongoose.connection;
 
 export default Connection;
 export { conn };
-
